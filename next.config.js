5ada9e83ea320a1164de0a6e9aae6643cc48c074
Wandago <48216682+Wandago@users.noreply.github.com>
2026-08-01 20:46:15 +0300

    TUMA CV V5

--- next.config.js
+++ next.config.js
@@ -21,6 +21,7 @@ const nextConfig = {
             value: [
               "default-src 'self'",
               "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+              "worker-src 'self' blob:",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: blob:",
